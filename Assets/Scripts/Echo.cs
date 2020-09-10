2f963a3d957be804b322f5370e4ef4c244858eb4
zhenyuan Pan <1656922908@qq.com>
2020-09-10 13:17:01 +0800

    一个Echo程序的实现

---
+++ Assets/Scripts/Echo.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//socket编程API所在命名空间
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine.UI;
+using System.Linq;
+public class Echo : MonoBehaviour
+{
+    //定义Socket
+    Socket socket;
+
+    private InputField inputField;
+    private Text seedText;
+    private Text receiveText;
+    private Button ConnectBtn;
+    private Button SendBtn;
+    private void Start()
+    {
+        inputField = GetComponentInChildren<InputField>();
+        seedText = inputField.transform.GetComponentInChildren<Text>();
+        //ConnectBtn = GetComponentsInChildren<Button>();
+        var btns = GetComponentsInChildren<Button>();
+        ConnectBtn = btns.ToList<Button>().Find(e => e.transform.name == "ConnectBtn");
+        SendBtn = btns.ToList<Button>().Find(e => e.transform.name == "SendBtn");
+        receiveText = GetComponentsInChildren<Text>().ToList().Find(e => e.transform.name == "ReceiveText");
+        ConnectBtn.onClick.AddListener(Connection);
+        SendBtn.onClick.AddListener(Seed);
+        receiveText.text = "";
+    }
+
+    //连接按钮
+    private void Connection() 
+    {
+        //socket 创建socket对象 三个参数是地址族,嵌套字类型,协议
+        socket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+        //connect 是一个阻塞方法: 程序会卡住直到服务器回应(接受, 拒绝或超时)
+        socket.Connect("127.0.0.1",19999);
+    }
+    //发送按钮
+    private void Seed() 
+    {
+        //Seed
+        string sendStr = inputField.text;
+        //将数据转换成byte数组
+        byte[] sendBytes = Encoding.Default.GetBytes(sendStr);
+        //客户端通过socket.Send发送数据.这也是一个阻塞方法。该方法接受一个bytes[]类型数组指明发送的内容 它的返回值指明发送数据的长度
+        socket.Send(sendBytes);
+        //Recv
+        byte[] readBuff = new byte[1024];
+        //客户端通过sockect.Receive接受客户端数据。他也是一个阻塞方法，没有收到服务端数据时程序会卡在Receive不会向下执行
+        //他有个bytes[]类型的参数 储存接受的数据 返回值则表示接受数据的长度
+        int count = socket.Receive(readBuff);
+        string recvStr = Encoding.Default.GetString(readBuff,0,count);
+        receiveText.text = recvStr;
+        //close 关闭连接
+        socket.Close();
+    }
+}
---
+++ EchoServer/EchoServer/EchoServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+namespace EchoServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Socket
+            Socket listenfd = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            //Bind 绑定 Socket.Bind绑定IP和端口。
+            //IPAddress指定IP地址
+            IPAddress ipAdr = IPAddress.Parse("127.0.0.1");
+            //IPEndPoint指定IP和端口
+            IPEndPoint ipEp = new IPEndPoint(ipAdr,19999);
+            listenfd.Bind(ipEp);
+            //Listen 监听：服务端通过socket.Listen(backlog)开始监听, 等待客户端连接。该参数指定队列中最多可荣达当代的连接数。0为不限制
+            listenfd.Listen(0);
+            Console.WriteLine("[服务器]启动成功");
+            //应答Accept 开启监听后 服务器调用socket.Accept接收客户端连接. 本例子使用的所有的Socket方法都是阻塞方法, 也就是说当没有客户端连接时, 服务器程序卡在Accept
+            //不会往下执行,直到接收了客户端的连接。Accept返回一个新客户端的Sockect对象， 对于服务器来说，它有一个监听Socket（listenfd）用来监听Listen和应答Accept客户端
+            //的连接,对于每个客户端还有一个专门的Socket(connfd)用来处理该客户端的数据
+            while (true)
+            {
+                //Accept
+                Socket connfd = listenfd.Accept();
+                Console.WriteLine("[服务器]Accpet");
+                //Receive
+                byte[] readBuff = new byte[1024];
+                int count = connfd.Receive(readBuff);
+                string readStr = System.Text.Encoding.Default.GetString(readBuff,0,count);
+                Console.WriteLine("[服务器接收]"+readStr);
+                //Send
+                byte[] sendBytes = System.Text.Encoding.Default.GetBytes(readStr);
+                connfd.Send(sendBytes);
+            }
+        }
+    }
+}
